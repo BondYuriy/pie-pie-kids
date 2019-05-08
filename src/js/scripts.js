890b2854e631c6875d23fba8cdbbe9e819cce5a7
BondYuriy <bondar.y@i.ua>
2019-05-08 21:27:27 +0300

    adds starting

---
+++ src/js/scripts.js
@@ -0,0 +1,35 @@
+$(function() {
+  $(window).scroll(function() {
+    var winTop = $(window).scrollTop();
+    if (winTop >= 100) {
+      $("body").addClass("sticky-header");
+    } else {
+      $("body").removeClass("sticky-header");
+    }
+  });
+});
+
+//================================================
+
+$(".navbar-toggler").click(function() {
+  $(this).toggleClass("open");
+});
+
+//=================================================
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 0,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+});
